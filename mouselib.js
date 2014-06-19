a9220ce7bf55c77fab1d4a1cf917fe42d4da4722
Nathan Lintz <nlintz@gmail.com>
2014-06-19 10:39:42 -0700

    wrote up some light docs about node obj c

--- mouselib.js
+++ mouselib.js
@@ -2,10 +2,30 @@ var $ = require('NodObjC')
 $.framework('Foundation')
 $.framework('CoreGraphics')
 
-var pool = $.NSAutoreleasePool('alloc')('init');
+// var pool = $.NSAutoreleasePool('alloc')('init');
+// pool('drain');
 
+/**
+  There are two ways to access objc functions - c calls or objective c message passing
+  
+  The node objC library exposes both (thanks to @TooTallNate For writing the beautiful NodObjC Lib)
+  Full API Docs for NodObjC can be found here: http://tootallnate.github.io/NodObjC/
 
-pool('drain');
+  Remember to import the correct libraries, we're using Foundation and Core Graphics in this example
+
+  Example: C Calls
+  Obj C: CGPoint CGPointMake (NSInteger X, NSInteger Y); -> Returns CGPoint struct
+  JS: $.CGPointMake(X, Y); -> Returns point object
+
+  Example: Obj C Message Passing
+  Obj C: NSString *string = [NSString stringWithUTF8String: @"Hello Objective-C World!"]
+  JS: var string = $.NSString('stringWithUTF8String', 'Hello Objective-C World!')
+
+  In General you use: object('messageNameWithArg', someArg, 'andArg', anotherArg)
+    Even args are the labels, odd args are the actual arguments
+
+**/
+
 
 function moveMouseToPosition (X, Y) {
   var mouse = $.CGEventCreateMouseEvent(null, $.kCGEventMouseMoved, $.CGPointMake( X, Y), 0);
